ae2154aff34ff699e85363e4c9c560b04a30189f
kalubre23 <luka.zola@hotmail.com>
2024-06-11 15:42:02 +0200

    dodat modal koji se otvara, zatvara i vraca podatke kad se book-uje mjesto

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ModalController, NavController } from '@ionic/angular';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
+import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -8,15 +11,45 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+  place: Place | undefined | any;
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  constructor(private route: ActivatedRoute, 
+    private navCtrl: NavController,
+    private modalCtrl: ModalController,
+    private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')){
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+      const id = paramMap.get('placeId');
+      if(id === null) {
+         console.log('Id is null');
+        return;
+      } else {
+        this.place = this.placesService.getPlace(id);
+      }
+    })
   }
 
   onBookPlace() {
     //ovo prikazuje losu animaciju ako je stack stranica prazan
     // this.router.navigateByUrl('/places/tabs/discover');
-    this.navCtrl.navigateBack('/places/tabs/discover');
+    //this.navCtrl.navigateBack('/places/tabs/discover');
+    this.modalCtrl.create(
+      {component: CreateBookingComponent, 
+      componentProps: {selectedPlace: this.place}}
+    ).then(modalEl => {
+      modalEl.present();
+      //on diddissmiss vraca obecanje tako da moze then
+      return modalEl.onDidDismiss();
+    }).then(resultData => {
+      console.log(resultData.data, resultData.role);
+      if (resultData.role === 'confirm'){
+        console.log('BOOKED!!!!!!');
+      }
+    })
   }
 }
